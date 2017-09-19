ae0295523f1c9d4131680b4c8405afeb62f17d5b
Claudius Mbemba <mbembac@gmail.com>
2017-09-19 15:00:32 -0700

    finished tutorial

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Keras Model Creatation Tutorial
+
+[keras-tutorial-deep-learning-in-python](https://elitedatascience.com/keras-tutorial-deep-learning-in-python)
---
+++ test.py
@@ -0,0 +1,34 @@
+import cv2
+from keras.models import load_model
+# from keras.utils import np_utils
+# from keras.datasets import mnist
+from keras.preprocessing import image
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# load model
+model = load_model("mnist.h5")
+
+# read in image
+img = image.load_img("./images/9-v.png")
+plt.imshow(img)
+# plt.show()
+
+# show channels
+x = image.img_to_array(img)
+print x.shape
+
+# conver to 1 channel gray scale
+x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
+print x.shape
+
+# convert to 4 channels: (nb_samples, nb_channels, width, height)
+x = x.reshape(1, 1, 28, 28)
+# x = np.expand_dims(x, axis=0)
+# x = np.reshape(x, (1, 28, 28, 1))
+print x.shape
+
+# predict
+prediction = model.predict(x)
+print np.around(prediction[0], decimals=0)
